431c7921bdcf83744be1d6617a53ee6c715b93d0
Max Kozyritsky <maxim.kozyricki@gmail.com>
2018-05-13 17:47:16 +0300

    added team js

--- project/team.js
+++ project/team.js
@@ -6,28 +6,25 @@ teamList.addEventListener("click", function (e) {
     e.preventDefault();
     if (e.target.classList.contains('teammate__name')) {
         let current = e.target;
-        let teammate = current.parentNode;
-        let item = teammate.parentNode;
+        let item = current.parentNode;
         let list = item.parentNode;
         let items = list.children;
         let teammateInfo = current.nextElementSibling;
-        console.log(teammateInfo);
         let teammateInfoHeight = teammateInfo.firstElementChild.clientHeight;
-        console.log(teammateInfoHeight);
 
-        //teammateInfo.style.height = teammateInfoHeight + 'px';
-        if(!teammate.classList.contains('teammates__item_active')){
+        if(!item.classList.contains('teammates__item_active')){
             for (let i = 0; i < items.length; i++){
                 items[i].classList.remove('teammates__item_active');
                 items[i].lastElementChild.style.height = 0;
-
-
             }
             item.classList.add('teammates__item_active');
             teammateInfo.style.height = teammateInfoHeight + 'px';
+        } else{
+            item.classList.remove('teammates__item_active');
+            teammateInfo.style.height = 0;
         }
     }
-})
+});
 
 
 
