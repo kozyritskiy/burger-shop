0e9a65912d2a4fcbb95a88071758efa7c82b9eb9
Max Kozyritsky <maxim.kozyricki@gmail.com>
2018-05-16 18:59:05 +0300

    added menu js

--- project/menu.js
+++ project/menu.js
@@ -1,13 +1,52 @@
-let smenuItem = document.querySelector(".smenu__item");
-let click = 0;
+let smenuList = document.querySelector(".smenu__list");
 
-smenuItem.addEventListener("click", function (e) {
+smenuList.addEventListener('click', function (e) {
     e.preventDefault();
-    smenuItem.classList.add("smenu__item_active");
-    click++;
-    if(click == 2){
-        click = 0;
-        smenuItem.classList.remove("smenu__item_active");
+
+    if (e.target.classList.contains('smenu__trigger')) {
+        let current = e.target;
+        let item = current.parentNode;
+        let list = item.parentNode;
+        let items = list.children;
+        let content = current.nextElementSibling;
+        let contentHeight = content.firstElementChild.clientHeight;
+
+        if (!item.classList.contains('smenu__item_active')) {
+            for (let i = 0; i < items.length; i++) {
+                items[i].classList.remove('smenu__item_active');
+            }
+            item.classList.add('smenu__item_active');
+            mobileMediaRequest(window.matchMedia("(max-width: 480px)").matches)
+                .then(function () {
+                    for (let i = 0; i < items.length; i++) {
+                        if (!items[i].classList.contains('smenu__item_active')) {
+                            items[i].classList.add("visuallyhidden");
+                        }
+                    }
+                    console.log('win');
+                });
+        } else {
+            item.classList.remove('smenu__item_active');
+            mobileMediaRequest(window.matchMedia("(max-width: 480px)").matches)
+                .then(function () {
+                    for (let i = 0; i < items.length; i++) {
+                        setTimeout(function () {
+                            items[i].classList.remove("visuallyhidden");
+                        },1000);
+
+                    }
+                    console.log('win again');
+                });
+        }
     }
 });
+
+function mobileMediaRequest(width) {
+    let promise = new Promise(function (resolve) {
+        if (width) {
+            resolve();
+        }
+    });
+    return promise;
+}
 
